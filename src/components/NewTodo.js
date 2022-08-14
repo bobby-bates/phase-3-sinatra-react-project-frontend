6b2d886f886c08397883c8c19d1210dc4b916a1b
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-13 21:06:01 -0400

    add POST fetch for new todo

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -6,8 +6,19 @@ export default function NewTodo({ todos, categories }) {
   const handleSubmit = e => {
     e.preventDefault()
 
-    
-  }
+    fetch('http://localhost:9292/todo', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        body: newTodoText,
+        // category_id: TODO: Do logic to find category_id
+      })
+    })
+      .then(r => r.json())
+      .then(newTodo => {
+        onAddTodo(newTodo)
+        setNewTodoText('')
+      })
 
   return (
     <form className='new-todo' onSubmit={handleSubmit}>
