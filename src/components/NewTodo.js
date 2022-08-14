aa2a2c199fe17a8300bea5cc7241e8c81581e959
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-14 11:06:11 -0400

    save new todo to server

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,27 +1,40 @@
 import { useState } from 'react'
 
-export default function NewTodo({ todos, categories, onAddTodo }) {
+export default function NewTodo({ todos, categories, onAddTodo}) {
   const [newTodoText, setNewTodoText] = useState('')
+  const [newTodoCategory, setNewTodoCategory] = useState('')
+
+  // NOTE: Not sure why onChange needs to pass categories into this
+  // rather than just calling categories.
+  const handleCategoryChange = e => {
+    setNewTodoCategory(categories.find(cat => cat.name === e.target.value))
+  }
 
   const handleSubmit = e => {
     e.preventDefault()
-
+    console.log('newTodoText:', newTodoText)
+    console.log('newTodoCategory', newTodoCategory)
+    // debugger
     fetch('http://localhost:9292/todo', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        body: newTodoText
-        // category_id: TODO: Do logic to find category_id
+        body: newTodoText,
+        category_id: newTodoCategory.id
       })
     })
       .then(r => r.json())
       .then(newTodo => {
+        debugger
+      // Update the todo list state:
         onAddTodo(newTodo)
+      // Reset form:
         setNewTodoText('')
+        setNewTodoCategory('')
       })
     }
 
-    const buildCategories = categories.map((cat, index) =>
+    const buildCategories = categories.map(cat =>
     <option key={cat.name} value={cat.name}>{cat.name}</option>
   )
 
@@ -44,7 +57,16 @@ export default function NewTodo({ todos, categories, onAddTodo }) {
         htmlFor='new-todo-categories-select'>
           Category:
       </label>
-      <select className='new-todo-form-child' id='new-todo-categories-select'>
+      <select
+        className='new-todo-form-child'
+        id='new-todo-categories-select'
+        value={newTodoCategory.name}
+        onChange={e => handleCategoryChange(e, categories)}
+        // onChange={e => {
+        //   debugger
+        //   setNewTodoCategory(e.target.value)}
+        // }
+      >
         <option value=''>Select:</option>
         {buildCategories}
       </select>
